e1e4d4977e80a5ce0b60f8e10aa5f5126884a4c6
Jonathan Sexton <joncsexton@gmail.com>
2018-07-26 12:13:42 -0500

    refactored starter code to ES6 syntax

--- js/app.js
+++ js/app.js
@@ -1,34 +1,44 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
+class Entity {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    // The image/sprite for our entities, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+    this.sprite = 'images/';
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
+    // Draw the enemy on the screen, required method for game
+    render() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    update(dt) {
+      // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+    }
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+  }
+}
+
+class Player extends Entity {
+  constructor() {
+    super();
+  }
+
+  handleInput(input) {
+    switch(input) {
+      
+    }
+  }
+}
+
+class Enemy extend Entity {
+  constructor() {
+    super();
+  }
+}
+
 
 
 
